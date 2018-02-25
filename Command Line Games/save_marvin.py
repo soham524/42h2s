dc10c3a32892aecd69f24898f68a5275e66bab14
Soham Rajadhyaksha <srajadhy@e1z2r3p35.42.us.org>
2018-02-24 17:07:50 -0800

    I'm an idiot

--- Command Line Games/save_marvin.py
+++ Command Line Games/save_marvin.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python
 import random
 import sys
-lives = 10 
+lives = 15
 num = 0
 i = 0
 underscore = []
-print "WARNING: CASE-SENSITIVE"
-mode = input("Enter easy, medium, or hard to choose the mode: ")
-
+word = []
+mode = raw_input("Enter easy, medium, or hard to choose the mode: ")
 if mode == "easy":
 	easyword = (random.choice(open("easy_mode.txt").readline().split()))
 	i = 0
 	while i < len(easyword):
 		print ("_"), 
 		i = i + 1 
-	
 elif mode == "medium":
 	mediumword = (random.choice(open("medium_mode.txt").readline().split()))
 	i = 0
@@ -26,20 +24,24 @@ elif mode == "hard":
 	print "Note: The first letter of the word is uppercase."
 	hardword = (random.choice(open("hard_mode.txt").readline().split()))
 	i = 0
+	word.append(hardword)
 	while i < len(hardword):
 		underscore.append("_")
 		i = i + 1 
-	"".join (underscore)
-	while i < lives:
+	print (underscore)
+	while lives > 0:
 			print hardword
 			guess = raw_input("LETTER: ")
-			pos = hardword.index(guess)
 			if guess in hardword:
+				pos = hardword.index(guess)
 				underscore[pos] = guess
-			else:
 				print underscore
+			else:
+				print ("That letter is not in the word")
+				print underscore
+				lives = lives - 1
 			i = i + 1
-			print ("remaining lives: " + str(lives-i))
+			print ("remaining lives: " + str(lives))
 				
  		
 
